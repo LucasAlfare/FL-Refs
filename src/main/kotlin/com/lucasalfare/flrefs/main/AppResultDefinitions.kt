cefb4af613bf57328816a0138fa28f175b206449
LucasAlfare <god-lucas@hotmail.com>
2024-05-06 13:11:42 -0300

    remove [AppResultDefinitions.kt]

--- src/main/kotlin/com/lucasalfare/flrefs/main/AppResultDefinitions.kt
+++
@@ -1,9 +0,0 @@
-package com.lucasalfare.flrefs.main
-
-interface AppError
-
-interface AppResult<out D, out E : AppError>
-
-data class Success<D, E : AppError>(val data: D) : AppResult<D, E>
-
-data class Failure<D, E : AppError>(val error: E) : AppResult<D, E>
