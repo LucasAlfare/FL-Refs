769391d1b2c5b48273f90a9599445406a041f706
LucasAlfare <god-lucas@hotmail.com>
2024-05-06 13:18:38 -0300

    added [@Deprecated] annotations to not used fields

--- src/main/kotlin/com/lucasalfare/flrefs/main/Misc.kt
+++ src/main/kotlin/com/lucasalfare/flrefs/main/Misc.kt
@@ -3,8 +3,10 @@ package com.lucasalfare.flrefs.main
 
 class Constants {
   companion object {
-    // fields just to quick change between databases
+    @Deprecated("Postgres URL are now handled by Environment variables.")
     const val POSTGRES_URL = "jdbc:postgresql://TTTT:0000/"
+
+    @Deprecated("Postgres URL are now handled by Environment variables.")
     const val POSTGRES_DRIVER = "org.postgresql.Driver"
 
     const val SQLITE_URL = "jdbc:sqlite:./data.db"
