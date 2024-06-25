a0ded354e45bc4b07bba0b0522ff22f247d3f3be
LucasAlfare <god-lucas@hotmail.com>
2024-06-25 00:32:09 -0300

    clean up to use dependency templates

--- src/main/kotlin/com/lucasalfare/flrefs/main/Misc.kt
+++ src/main/kotlin/com/lucasalfare/flrefs/main/Misc.kt
@@ -6,19 +6,6 @@ import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import javax.imageio.ImageIO
 
-//class Constants {
-//  companion object {
-//    @Deprecated("Postgres URL are now handled by Environment variables.")
-//    const val POSTGRES_URL = "jdbc:postgresql://TTTT:0000/"
-//
-//    @Deprecated("Postgres URL are now handled by Environment variables.")
-//    const val POSTGRES_DRIVER = "org.postgresql.Driver"
-//
-//    const val SQLITE_URL = "jdbc:sqlite:./data.db"
-//    const val SQLITE_DRIVER = "org.sqlite.JDBC"
-//  }
-//}
-
 class ImageUtil {
 
   companion object {
