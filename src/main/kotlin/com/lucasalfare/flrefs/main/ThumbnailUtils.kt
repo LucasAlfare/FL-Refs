44e4d809d232f4213798e91c05ad44a3124fbc5b
LucasAlfare <god-lucas@hotmail.com>
2024-07-07 23:43:43 -0300

    migrate thumbnail generation to use `Thumbnailator` dependency.

--- src/main/kotlin/com/lucasalfare/flrefs/main/ThumbnailUtils.kt
+++ src/main/kotlin/com/lucasalfare/flrefs/main/ThumbnailUtils.kt
@@ -1,23 +1,12 @@
 package com.lucasalfare.flrefs.main
 
-import java.awt.Image
-import java.awt.image.BufferedImage
+import net.coobird.thumbnailator.Thumbnailator
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
-import javax.imageio.ImageIO
 
 fun generateThumbnail(imageBytes: ByteArray, width: Int = 200, height: Int = 200): ByteArray {
-  val originalImage = ImageIO.read(ByteArrayInputStream(imageBytes))
-  val resizedImage = originalImage.getScaledInstance(width, height, Image.SCALE_SMOOTH)
-  val bufferedImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
-
-  bufferedImage.createGraphics().apply {
-    drawImage(resizedImage, 0, 0, null)
-    dispose()
-  }
-
-  return ByteArrayOutputStream().use { outputStream ->
-    ImageIO.write(bufferedImage, "jpg", outputStream)
-    outputStream.toByteArray()
+  return ByteArrayOutputStream().use {
+    Thumbnailator.createThumbnail(ByteArrayInputStream(imageBytes), it, width, height)
+    it.toByteArray()
   }
 }
