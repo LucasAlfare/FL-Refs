69c14ce6f2964a11d9fe61e6ac32546d131033d9
LucasAlfare <god-lucas@hotmail.com>
2024-07-07 22:53:56 -0300

    clean up

--- src/main/kotlin/com/lucasalfare/flrefs/main/ThumbnailUtils.kt
+++ src/main/kotlin/com/lucasalfare/flrefs/main/ThumbnailUtils.kt
@@ -7,17 +7,17 @@ import java.io.ByteArrayOutputStream
 import javax.imageio.ImageIO
 
 fun generateThumbnail(imageBytes: ByteArray, width: Int = 200, height: Int = 200): ByteArray {
-  val originalImage: BufferedImage = ImageIO.read(ByteArrayInputStream(imageBytes))
-  val resizedImage: Image = originalImage.getScaledInstance(width, height, Image.SCALE_DEFAULT)
+  val originalImage = ImageIO.read(ByteArrayInputStream(imageBytes))
+  val resizedImage = originalImage.getScaledInstance(width, height, Image.SCALE_SMOOTH)
   val bufferedImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
-  val graphics = bufferedImage.createGraphics()
-  graphics.drawImage(resizedImage, 0, 0, null)
-  graphics.dispose()
 
-  val outputStream = ByteArrayOutputStream()
-  // we use jpg format to really compress the image
-  ImageIO.write(bufferedImage, "jpg", outputStream)
-  outputStream.close()
+  bufferedImage.createGraphics().apply {
+    drawImage(resizedImage, 0, 0, null)
+    dispose()
+  }
 
-  return outputStream.toByteArray()
+  return ByteArrayOutputStream().use { outputStream ->
+    ImageIO.write(bufferedImage, "jpg", outputStream)
+    outputStream.toByteArray()
+  }
 }
