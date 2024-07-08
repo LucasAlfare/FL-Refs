e029586fc8a84a2d0e86dee4cf1d15c80a1183ee
LucasAlfare <god-lucas@hotmail.com>
2024-07-07 23:51:45 -0300

    clean up

--- src/main/kotlin/com/lucasalfare/flrefs/main/ThumbnailUtils.kt
+++ src/main/kotlin/com/lucasalfare/flrefs/main/ThumbnailUtils.kt
@@ -1,12 +1,17 @@
 package com.lucasalfare.flrefs.main
 
-import net.coobird.thumbnailator.Thumbnailator
+import net.coobird.thumbnailator.Thumbnailator.createThumbnail
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 
 fun generateThumbnail(imageBytes: ByteArray, width: Int = 200, height: Int = 200): ByteArray {
-  return ByteArrayOutputStream().use {
-    Thumbnailator.createThumbnail(ByteArrayInputStream(imageBytes), it, width, height)
-    it.toByteArray()
+  return ByteArrayOutputStream().use { outputStream ->
+    createThumbnail(
+      ByteArrayInputStream(imageBytes),
+      outputStream,
+      width,
+      height
+    )
+    outputStream.toByteArray()
   }
 }
