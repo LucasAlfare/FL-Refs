21d4d839aa26050d9de6490016c045cfe9383cc0
LucasAlfare <god-lucas@hotmail.com>
2024-07-16 02:07:37 -0300

    update clear all tables route

--- backend/src/main/kotlin/com/lucasalfare/flrefs/main/routes/ClearAllRoute.kt
+++ backend/src/main/kotlin/com/lucasalfare/flrefs/main/routes/ClearAllRoute.kt
@@ -1,13 +1,12 @@
 package com.lucasalfare.flrefs.main.routes
 
 import com.lucasalfare.flrefs.main.data.exposed.AppDB
-import com.lucasalfare.flrefs.main.data.exposed.ImagesInfos
-import com.lucasalfare.flrefs.main.data.exposed.ImagesUrls
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import org.jetbrains.exposed.sql.deleteAll
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 
 /**
  * Defines a route for clearing all items in the database.
@@ -19,20 +18,14 @@ import org.jetbrains.exposed.sql.deleteAll
 fun Routing.clearAllItemsRoute() {
   // TODO: this MUST be inside auth block!
   delete("/clear") {
-    AppDB.exposedQuery {
-      ImagesInfos.deleteAll()
-      ImagesUrls.deleteAll()
-      // TODO: include call to CDN clear
-    }.let {
-      when {
-        it >= 0 -> {
-          return@delete call.respond(HttpStatusCode.OK, "All tables were cleared.")
-        }
-
-        else -> {
-          return@delete call.respond(HttpStatusCode.InternalServerError, "Nothing was cleared.")
+    return@delete AppDB.exposedQuery {
+      transaction {
+        SchemaUtils.listTables().forEach {
+          exec("DROP TABLE IF EXISTS $it")
         }
       }
+      // TODO: include call to CDN clear
+      call.respond(HttpStatusCode.OK, "All tables were cleared.")
     }
   }
 }
